94adb4cc4d2b0f0239112805cc3fb3db392a3311
jonghopark95 <jong951005@gmail.com>
2021-03-21 19:00:59 +0900

    chore: add lint command and ignore list

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,15 @@ module.exports = {
     sourceType: "module", // fix Parsing error: The keyword 'import' is reserved
   },
   env: {
+    browser: true,
     node: true, // fix the "module" is not defined and "process" is not defined error.
   },
   rules: {
-    "react/prop-types": false,
+    "react/prop-types": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
