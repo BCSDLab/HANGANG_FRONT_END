31628cc7b51851cc5eb8b9a93126126fe46a2578
jonghopark95 <jong951005@gmail.com>
2021-03-22 21:22:33 +0900

    chore: add dependency
    알림을 위한 react-toast-notifications 라이브러리를 추가하였습니다.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import App from "App";
-import GlobalStyle from "GlobalStyle";
 import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store";
+import { ToastProvider } from "react-toast-notifications";
+import store from "store";
+import App from "App";
+import GlobalStyle from "GlobalStyle";
 
 ReactDom.render(
   <Provider store={store}>
-    <GlobalStyle />
-    <App />
+    <ToastProvider>
+      <GlobalStyle />
+      <App />
+    </ToastProvider>
   </Provider>,
   document.getElementById("root")
 );
