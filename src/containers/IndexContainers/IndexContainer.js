e608eb6682cab45e0dcda60cb61c3fb6a47a0b59
jonghopark95 <jong951005@gmail.com>
2021-03-29 19:53:33 +0900

    feat: add auto login logic
    자동 로그인을 위한

---
+++ src/containers/IndexContainers/IndexContainer.js
@@ -0,0 +1,106 @@
+import React, { useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+
+import AuthAPI from "api/auth";
+import {
+  getValueOnLocalStorage,
+  removeValueOnLocalStorage,
+} from "utils/localStorageUtils";
+import { succeedTokenCheck } from "store/modules/auth";
+import { useToasts } from "react-toast-notifications";
+
+const StyledLink = styled(Link)``;
+
+const IndexContainer = () => {
+  const { addToast } = useToasts();
+  const history = useHistory();
+  const { didTokenCheck, isLoggedIn } = useSelector((state) => state.authReducer);
+  const dispatch = useDispatch();
+
+  // 참고 : https://app.diagrams.net/#G1fdPJc3IfiFc6l8OMxSJ2rOJisJsp0k8i
+  const refreshAccessToken = async (token) => {
+    try {
+      const res = await AuthAPI.refreshToken(token.refresh_token);
+      if (res.status === 200) {
+        dispatch(succeedTokenCheck({ isLoggedIn: true, token: token }));
+      }
+    } catch (err) {
+      if (err.response.data.code === 6) {
+        removeValueOnLocalStorage("token");
+        history.push("/login");
+        addToast("토큰이 유효하지 않습니다. 다시 로그인 해주세요.", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+      }
+
+      if (err.response.data.code === 9) {
+        removeValueOnLocalStorage("token");
+        history.push("/login");
+        addToast(
+          "2주 이내로 로그인 하지 않아 토큰이 만료되었습니다. 다시 로그인 해주세요.",
+          {
+            appearance: "error",
+            autoDismiss: true,
+          }
+        );
+      }
+    }
+  };
+
+  const checkValidAccessToken = async (token) => {
+    try {
+      const res = await AuthAPI.authTest(token.access_token);
+      if (res.status === 200) {
+        dispatch(succeedTokenCheck({ isLoggedIn: true, token: token }));
+      }
+    } catch (err) {
+      if (err.response.data.code === 5) {
+        removeValueOnLocalStorage("token");
+        history.push("/login");
+        addToast("토큰이 유효하지 않습니다. 다시 로그인 해주세요.", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+      }
+
+      if (err.response.data.code === 8) {
+        refreshAccessToken(token);
+      }
+    }
+  };
+
+  const checkTokenOnLocalStorage = () => {
+    let token = getValueOnLocalStorage("token");
+    if (!token) {
+      dispatch(succeedTokenCheck({ isLoggedIn: false, token: "" }));
+    } else {
+      checkValidAccessToken(token);
+    }
+  };
+
+  useEffect(() => {
+    if (!didTokenCheck) {
+      checkTokenOnLocalStorage();
+    }
+  }, []);
+
+  return (
+    <>
+      {didTokenCheck && (
+        <>
+          {!isLoggedIn && (
+            <StyledLink to="/login">
+              <span>로그인</span>
+            </StyledLink>
+          )}
+          {isLoggedIn && <span>로그아웃</span>}
+        </>
+      )}
+    </>
+  );
+};
+
+export default IndexContainer;
---
+++ src/pages/IndexPage.js
@@ -0,0 +1,8 @@
+import IndexContainer from "containers/IndexContainers/IndexContainer";
+import React from "react";
+
+const IndexPage = () => {
+  return <IndexContainer />;
+};
+
+export default IndexPage;
