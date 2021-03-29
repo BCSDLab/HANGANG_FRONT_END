6f200b6a30649b02c9966f22feb88f4fe587f699
jonghopark95 <jong951005@gmail.com>
2021-03-29 19:48:13 +0900

    style: page rename

--- src/pages/AuthPages/EmailAuthPage.js
+++
@@ -1,8 +0,0 @@
-import EmailAuthContainer from "containers/AuthContainers/EmailAuthContainer";
-import React from "react";
-
-const EmailAuthPage = () => {
-  return <EmailAuthContainer />;
-};
-
-export default EmailAuthPage;
---
+++ src/pages/AuthPages/FindPwPage.js
@@ -0,0 +1,8 @@
+import NewPwContainer from "containers/AuthContainers/NewPwContainer";
+import React from "react";
+
+const FindPwPage = () => {
+  return <NewPwContainer />;
+};
+
+export default FindPwPage;
