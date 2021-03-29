736f214567eb70d58e69eb0253837735b1ca0623
jonghopark95 <jong951005@gmail.com>
2021-03-29 19:54:47 +0900

    feat: remove token if not checked autologin
    자동 로그인이 false일 경우 브라우저에서 나가기 전 토큰을 지우는 로직을 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginPage from "pages/AuthPages/LoginPage";
+import FindPwPage from "pages/AuthPages/FindPwPage";
+import FindPwAuthPage from "pages/AuthPages/FindPwAuthPage";
 import SignUpPage from "pages/AuthPages/SignUpPage";
-import EmailAuthPage from "pages/AuthPages/EmailAuthPage";
+import SignUpAuthPage from "pages/AuthPages/SignUpAuthPage";
+import IndexPage from "pages/IndexPage";
+import {
+  getValueOnLocalStorage,
+  removeValueOnLocalStorage,
+} from "utils/localStorageUtils";
 
 const Main = styled.main`
   height: 100%;
 `;
 
 const App = () => {
+  useEffect(() => {
+    window.addEventListener("beforeunload", () => {
+      const autoLoginKey = getValueOnLocalStorage("didHangangAutoLogin");
+      if (!autoLoginKey) {
+        removeValueOnLocalStorage("token");
+      }
+    });
+    return () => {
+      window.removeEventListener("beforeunload", () => {
+        const autoLoginKey = getValueOnLocalStorage("didHangangAutoLogin");
+        if (!autoLoginKey) {
+          removeValueOnLocalStorage("token");
+        }
+      });
+    };
+  }, []);
+
   return (
     <Main role="main">
       <Router>
         <Switch>
+          <Route path="/" exact component={IndexPage} />
           <Route path="/login" component={LoginPage} />
-          <Route path="/emailauth" component={EmailAuthPage} />
+          <Route path="/findpwauth" component={FindPwAuthPage} />
+          <Route path="/findpw" component={FindPwPage} />
+          <Route path="/signupauth" component={SignUpAuthPage} />
           <Route path="/signup" component={SignUpPage} />
         </Switch>
       </Router>
