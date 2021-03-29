8e9272c77fdb4c811c43d5c1691465958a9def5e
jonghopark95 <jong951005@gmail.com>
2021-03-29 19:45:31 +0900

    style: find password page publishing

---
+++ src/pages/AuthPages/FindPwAuthPage.js
@@ -0,0 +1,8 @@
+import EmailAuthContainer from "containers/AuthContainers/EmailAuthContainer";
+import React from "react";
+
+const FindPwAuthPage = () => {
+  return <EmailAuthContainer findForWhat="findpw" />;
+};
+
+export default FindPwAuthPage;
