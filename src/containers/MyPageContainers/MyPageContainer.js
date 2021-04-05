48476455d070e0b84de6399f36029c1859455a98
jonghopark95 <jong951005@gmail.com>
2021-04-05 18:34:54 +0900

    fix: fix error when user access with expired token

--- src/containers/MyPageContainers/MyPageContainer.js
+++ src/containers/MyPageContainers/MyPageContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useToasts } from "react-toast-notifications";
 import styled from "styled-components";
@@ -16,6 +16,7 @@ import PurchasedSection from "components/MyPageComponents/PurchasedSection";
 import LoadingSpinner from "components/Shared/LoadingSpinner";
 
 import SettingSectionContainer from "./SettingSectionContainer";
+import { logout } from "store/modules/auth";
 
 const minHeight = "798px";
 
@@ -46,6 +47,7 @@ const MyPageContainer = () => {
   const { addToast } = useToasts();
   const { isCheckedToken, isLoggedIn } = useSelector((state) => state.authReducer);
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const [current, setCurrent] = useState("pointRecords");
   const [isLoaded, setIsLoaded] = useState(false);
@@ -62,6 +64,9 @@ const MyPageContainer = () => {
     errorCode: "",
   });
 
+  /**
+   * 유저가 token이 없이 접근할 경우 홈으로 내보냅니다.
+   */
   let accessToken;
   if (getValueOnLocalStorage("hangangToken") === null) {
     kickOut(1);
@@ -69,6 +74,17 @@ const MyPageContainer = () => {
     accessToken = getValueOnLocalStorage("hangangToken").access_token;
   }
 
+  /**
+   * 유저가 로그인 하지 않고 url로 접근할 경우 홈으로 내보냅니다.
+   *
+   * 만약 로그인이 되어있다면, 마이페이지에 필요한 API들을 요청합니다.
+   * getAmountOfActivity : 우상단 사용자 활동 수들
+   * getInfo : 사용자 정보
+   * getPointRecords : 사용자 포인트 사용 내역
+   *
+   * 모든 요청이 끝나면 setIsLoaded를 true로 해준다.
+   * isLoaded가 false 인 경우 로딩 스피너를 보여준다.
+   */
   useEffect(async () => {
     if (isCheckedToken && !isLoggedIn) {
       kickOut(1);
@@ -76,37 +92,31 @@ const MyPageContainer = () => {
       try {
         const { data: activityAmount } = await MypageAPI.getAmountOfActivity(accessToken);
         const { data: infoDatas } = await MypageAPI.getInfo(accessToken);
+        const { data: pointRecords } = await MypageAPI.getPointRecords(accessToken);
         setUserInfo({ activityAmount, infoDatas });
+        setPointRecords(pointRecords);
         setNicknameTest((prev) => ({
           ...prev,
           currentNickname: infoDatas.nickname,
         }));
       } catch (error) {
-        kickOut(error.response.data.code);
+        dispatch(logout({ errorCode: error.response.data.code }));
+        addToast("토큰이 만료되었습니다. 다시 로그인 해주세요.", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+        history.push("/");
       } finally {
         setIsLoaded(true);
       }
     }
   }, [isCheckedToken, isLoggedIn]);
 
+  /**
+   * 구매한 강의자료, 내 스크랩을 누를 경우 이에 맞는 API를 호출합니다.
+   */
   useEffect(async () => {
     switch (current) {
-      case "pointRecords":
-        if (pointRecords.length === 0) {
-          try {
-            const { data } = await MypageAPI.getPointRecords(accessToken);
-            setPointRecords(data);
-          } catch (error) {
-            if (error.response.data.code === 5) {
-              history.push("/");
-              addToast("토큰이 만료되었습니다. 다시 로그인 해주세요.", {
-                appearance: "error",
-                autoDismiss: true,
-              });
-            }
-          }
-        }
-        break;
       case "purchased":
         if (purchased.length === 0) {
           try {
