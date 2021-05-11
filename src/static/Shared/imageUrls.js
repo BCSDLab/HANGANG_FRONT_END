60b519c0bd9892b74bc28e69f817017ad8384401
jonghopark95 <jong951005@gmail.com>
2021-05-11 15:04:30 +0900

    fix: Update file icon path

--- src/static/Shared/imageUrls.js
+++ src/static/Shared/imageUrls.js
@@ -12,6 +12,6 @@ export const SEARCH_ICON_URL =
 export const ADD_FILE_ICON_URL =
   "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/resourcepage/add_file_icon.png";
 export const UPLOAD_FILE_ICON_URL = (type) =>
-  `https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/resourcepage/fileIcon/${type}.png`;
+  `https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/resourcepage/extension/${type}.png`;
 export const FILE_ERASE_BUTTON_URL =
-  "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/resourcepage/fileIcon/file_erase_button.png";
+  "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/resourcepage/file_erase_button.png";
