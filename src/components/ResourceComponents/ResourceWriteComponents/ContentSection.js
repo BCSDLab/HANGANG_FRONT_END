a5feef9ce22110a5d2d91ef56472257d075e564f
jonghopark95 <jong951005@gmail.com>
2021-05-19 16:34:14 +0900

    fix: Erase setForm props on content section

--- src/components/ResourceComponents/ResourceWriteComponents/ContentSection.js
+++ src/components/ResourceComponents/ResourceWriteComponents/ContentSection.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import { FontColor } from "static/Shared/commonStyles";
+import { useDispatch } from "react-redux";
+import { setForm } from "store/modules/resourceCreateModule";
 
 const Placeholder = `자료에 대한 설명을 작성해주세요. (저작권법 제 133조에 따라 타인 또는 유포금지 자료를 업로드할 경우,
 
@@ -38,23 +40,24 @@ const Content = styled.textarea.attrs({
   }
 `;
 
-const ContentSection = ({ content, setForm }) => (
-  <Wrapper>
-    <Content
-      value={content}
-      onChange={(e) => setForm((prev) => ({ ...prev, content: e.target.value }))}
-    />
-  </Wrapper>
-);
+const ContentSection = ({ content }) => {
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <Content
+        value={content}
+        onChange={(e) => dispatch(setForm("content", e.target.value))}
+      />
+    </Wrapper>
+  );
+};
 
 ContentSection.defaultProps = {
   content: "",
-  setForm: () => {},
 };
 
 ContentSection.propTypes = {
   content: PropTypes.string,
-  setForm: PropTypes.func,
 };
 
 export default ContentSection;
