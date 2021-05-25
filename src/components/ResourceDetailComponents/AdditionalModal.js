9b73cef1dc332d6631c6b969acec0ec8a45f8481
jonghopark95 <jong951005@gmail.com>
2021-05-25 13:12:28 +0900

    refactor: Create report util
    reportutil을 만들어 신고를 필요로 하는 컴포넌트는 해당 유틸함수를 사용하도록 수정하였습니다.

--- src/components/ResourceDetailComponents/AdditionalModal.js
+++ src/components/ResourceDetailComponents/AdditionalModal.js
@@ -4,11 +4,10 @@ import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import ResourceDetailAPI from "api/resourceDetail";
-import ALERT_MESSAGE_ON_ERROR_TYPE from "static/Shared/ALERT_MESSAGE_ON_ERROR_TYPE";
-import { showConfirmModal, showReportModal } from "store/modules/modalModule";
 import { scrapResource, unscrapResource } from "store/modules/resourceDetailModule";
 import { ModalWrapper, Report, Scrap } from "./styles/AdditionalModal.style";
 import { getValueOnLocalStorage } from "utils/localStorageUtils";
+import { triggerWhenNotLoggedIn, callReportModal } from "utils/reportUtils";
 
 AdditionalModal.propTypes = {
   contentId: PropTypes.string.isRequired,
@@ -28,42 +27,20 @@ function AdditionalModal({
   const { user_scrap_id } = useSelector((state) => state.resourceDetailReducer);
   const isAuthenticated = !isLoggedIn && isCheckedToken ? false : true;
 
-  const requestScrap = async () => {
-    try {
-      const accessToken = getValueOnLocalStorage("hangangToken").access_token;
-      let data;
-      let prevScrappedInfo = isScrapped;
-      if (isScrapped)
-        data = await ResourceDetailAPI.unscrapResource(
-          parseInt(user_scrap_id),
-          accessToken
-        );
-      else data = await ResourceDetailAPI.scrapResource(contentId, accessToken);
-
-      if (data.data.httpStatus === "OK") {
-        if (!prevScrappedInfo) dispatch(scrapResource({ contentId }));
-        else dispatch(unscrapResource());
-      }
-      console.log(isScrapped);
-      console.dir(data);
-    } catch (error) {
-      console.dir(error);
-    }
-  };
-
-  const handleReportClick = () => {
-    if (!isAuthenticated) triggerWhenNotLoggedIn(dispatch, history);
-    else dispatch(showReportModal({ contentId, type: "resource" }));
-  };
-
   const handleScrapClick = () => {
     if (!isAuthenticated) triggerWhenNotLoggedIn(dispatch, history);
-    else requestScrap();
+    else requestScrap(isScrapped, user_scrap_id, contentId, dispatch);
   };
 
   return (
     <ModalWrapper>
-      <Report onClick={() => handleReportClick()}>신고</Report>
+      <Report
+        onClick={() =>
+          callReportModal("resource", contentId, isAuthenticated, history, dispatch)
+        }
+      >
+        신고
+      </Report>
       <Scrap onClick={() => handleScrapClick()}>
         {isScrapped ? "스크랩 취소" : "스크랩"}
       </Scrap>
@@ -71,13 +48,27 @@ function AdditionalModal({
   );
 }
 
-/**
- * 로그인 되어있지 않은 유저가 기능에 접근할 시 로그인 알람을 띄웁니다.
- */
-const triggerWhenNotLoggedIn = (dispatch, history) => {
-  const { title, content } = ALERT_MESSAGE_ON_ERROR_TYPE["notLoggedIn"];
-  const onConfirm = () => history.push("/login");
-  dispatch(showConfirmModal({ title, content, onConfirm }));
+const requestScrap = async (isScrapped, user_scrap_id, contentId, dispatch) => {
+  try {
+    const accessToken = getValueOnLocalStorage("hangangToken").access_token;
+    let data;
+    let prevScrappedInfo = isScrapped;
+    if (isScrapped)
+      data = await ResourceDetailAPI.unscrapResource(
+        parseInt(user_scrap_id),
+        accessToken
+      );
+    else data = await ResourceDetailAPI.scrapResource(contentId, accessToken);
+
+    if (data.data.httpStatus === "OK") {
+      if (!prevScrappedInfo) dispatch(scrapResource({ contentId }));
+      else dispatch(unscrapResource());
+    }
+    console.log(isScrapped);
+    console.dir(data);
+  } catch (error) {
+    console.dir(error);
+  }
 };
 
 export default AdditionalModal;
