186172a593f4c1fa8521c658eaf2bc2b9add8a25
jonghopark95 <jong951005@gmail.com>
2021-05-25 15:10:15 +0900

    refactor: Move out style code on resource container

--- src/containers/ResourcesContainers/ResourceContainer.js
+++ src/containers/ResourcesContainers/ResourceContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
+
 import { debounce } from "lodash";
 
 import ResourceAPI from "api/resources";
@@ -21,115 +21,21 @@ import {
   setResourcesNextPage,
 } from "store/modules/resourcesModule";
 import ResourceFilterList from "static/ResourcesPage/ResourceFilterList.json";
-import {
-  BorderColor,
-  ConceptColor,
-  FontColor,
-  InnerContentWidth,
-  PlaceholderColor,
-} from "static/Shared/commonStyles";
+
 import { getValueOnLocalStorage } from "utils/localStorageUtils";
-
-const Wrapper = styled.div`
-  width: ${InnerContentWidth};
-  margin: 90px auto 98px auto;
-`;
-
-const SpinnerWrapper = styled.div`
-  width: 100%;
-  height: 1084px;
-  display: flex;
-  align-items: center;
-`;
-
-const SearchSection = styled.section`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-`;
-
-const FilterButton = styled.button`
-  all: unset;
-  width: 70px;
-  height: 32px;
-  border-radius: 16px;
-  color: ${({ isChosen }) => (isChosen ? `#fcfcfe` : `${FontColor}`)};
-  background-color: ${({ isChosen }) =>
-    isChosen ? `${ConceptColor}` : `${BorderColor}`};
-  font-size: 15px;
-  cursor: pointer;
-  text-align: center;
-`;
-
-const FilterSection = styled.section`
-  position: relative;
-  display: flex;
-  width: 100%;
-  height: 32px;
-
-  margin-top: 48px;
-
-  ${FilterButton}:not(:last-child) {
-    margin-right: 16px;
-  }
-`;
-
-const FilterImage = styled.img.attrs({
-  src:
-    "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/lecturespage/filter.png",
-  alt: "filter",
-})`
-  position: absolute;
-  right: 0;
-  width: 24px;
-  height: 24px;
-  cursor: pointer;
-`;
-
-const ResourcesSection = styled.section`
-  position: relative;
-  width: 100%;
-  padding: 24px 0px 98px 0px;
-`;
-
-const SearchResultLabel = styled.label`
-  display: block;
-  margin-bottom: 24px;
-
-  color: ${FontColor};
-  font-size: 20px;
-  font-weight: 500;
-`;
-
-const ResourceWriteButton = styled.input.attrs({
-  type: "button",
-  value: "강의자료 작성 >",
-})`
-  all: unset;
-  position: absolute;
-  top: calc(0% + 21px);
-  right: 0;
-  width: 101px;
-  height: 28px;
-  border-radius: 18.5px;
-  border: solid 1px ${ConceptColor};
-
-  font-size: 14px;
-  padding-left: 16px;
-  color: ${PlaceholderColor};
-  cursor: pointer;
-`;
-
-const CardGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: repeat(4, 133px);
-  grid-gap: 30px 18px;
-
-  width: 1152px;
-`;
-
-const FakeDiv = styled.div``;
+import {
+  CardGrid,
+  FakeDiv,
+  FilterButton,
+  FilterImage,
+  FilterSection,
+  ResourcesSection,
+  ResourceWriteButton,
+  SearchResultLabel,
+  SearchSection,
+  SpinnerWrapper,
+  Wrapper,
+} from "./styles/ResourceContainer.style";
 
 const ResourceContainer = () => {
   const dispatch = useDispatch();
