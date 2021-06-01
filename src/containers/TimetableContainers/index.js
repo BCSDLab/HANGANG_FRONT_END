f1fa8ebf2569bbd6fd97f233b7a487deb97bbedc
jonghopark95 <jong951005@gmail.com>
2021-06-02 07:42:32 +0900

    feat: Create Timetable container and page

---
+++ src/containers/TimetableContainers/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TimetableContainer = () => {
+  return (
+    <div>
+      <span>ㅎㅏ이</span>
+      <span>HI</span>
+    </div>
+  );
+};
+
+export default TimetableContainer;
---
+++ src/pages/TimeTablePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TimetableContainer from "containers/TimetableContainers";
+
+const TimetablePage = () => {
+  return <TimetableContainer />;
+};
+
+export default TimetablePage;
