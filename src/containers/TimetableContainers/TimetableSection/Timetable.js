f5300202b575193a99b8b606f8ec863af5895703
jonghopark95 <jong951005@gmail.com>
2021-06-07 13:57:27 +0900

    chore: Create dummy timetable

---
+++ src/containers/TimetableContainers/TimetableSection/Timetable.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import { DUMMY_TIMETABLE_URL } from "static/Shared/imageUrls";
+
+const TimetableWrapper = styled.div`
+  width: 560px;
+  margin-top: 24px;
+`;
+
+const DummyTimetable = styled.img.attrs({
+  src: DUMMY_TIMETABLE_URL,
+  alt: "dummy",
+})`
+  width: 100%;
+`;
+
+const Timetable = () => {
+  return (
+    <TimetableWrapper>
+      <DummyTimetable />
+    </TimetableWrapper>
+  );
+};
+
+export default Timetable;
--- src/containers/TimetableContainers/TimetableSection/index.js
+++ src/containers/TimetableContainers/TimetableSection/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { MORE_URL, DUMMY_TIMETABLE_URL } from "static/Shared/imageUrls";
+import { MORE_URL } from "static/Shared/imageUrls";
+import Timetable from "./Timetable";
 
 const TimetableAddBox = styled.section`
   position: relative;
@@ -12,6 +13,7 @@ const TimetableSelectBar = styled.div`
   width: 240px;
   height: 30px;
   background-color: #fff;
+  cursor: pointer;
 `;
 
 const TimetableSettingButton = styled.img.attrs({
@@ -22,19 +24,15 @@ const TimetableSettingButton = styled.img.attrs({
   width: 24px;
   top: 0;
   right: 0;
+  cursor: pointer;
 `;
-
-const DummyTimetable = styled.img.attrs({
-  src: DUMMY_TIMETABLE_URL,
-  alt: "dummy",
-})``;
 
 const TimetableSection = () => {
   return (
     <TimetableAddBox>
       <TimetableSelectBar />
       <TimetableSettingButton />
-      <DummyTimetable />
+      <Timetable />
     </TimetableAddBox>
   );
 };
