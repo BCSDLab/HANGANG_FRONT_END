e9b889bfde008b8d15e303896c5c27c9042b2529
jonghopark95 <jong951005@gmail.com>
2021-06-11 15:55:16 +0900

    feat: Show confirm modal when user access timetable page with not logged in

--- src/containers/TimetableContainers/index.js
+++ src/containers/TimetableContainers/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 import ChangingSemesterBar from "components/TimetableComponents/ChangingSemesterBar";
 import AddLectureSection from "containers/TimetableContainers/AddLectureSection";
 import TimetableSection from "containers/TimetableContainers/TimetableSection";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { triggerWhenNotLoggedIn } from "utils/reportUtils";
 
 const Background = styled.div`
   display: flex;
@@ -22,6 +25,16 @@ const MainContentsWrapper = styled.div`
 `;
 
 const TimetablePageContainer = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { isLoggedIn, isCheckedToken } = useSelector((state) => state.authReducer);
+
+  useEffect(() => {
+    if (isCheckedToken && !isLoggedIn) {
+      triggerWhenNotLoggedIn({ history, dispatch });
+    }
+  });
+
   return (
     <Background>
       <ChangingSemesterBar />
