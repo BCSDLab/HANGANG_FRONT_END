3413d74738f14d6ea01bda0a27bc497fd09acb8d
jonghopark95 <jong951005@gmail.com>
2021-06-13 18:39:35 +0900

    chore: Change class time array to class names

--- src/components/TimetableComponents/Lecture.js
+++ src/components/TimetableComponents/Lecture.js
@@ -19,6 +19,7 @@ import {
 } from "./styles/Lecture.style";
 import ALERT_MESSAGE_ON_ERROR_TYPE from "static/Shared/ALERT_MESSAGE_ON_ERROR_TYPE";
 import { showAlertModal } from "store/modules/modalModule";
+import { distributeClassTime } from "utils/timetablePage/distributeClassTime";
 
 const Lecture = ({ infos }) => {
   const history = useHistory();
@@ -47,7 +48,7 @@ const Lecture = ({ infos }) => {
         {infos.grades}학점 &nbsp;
         {infos.target.length === 2 ? "전체" : `${infos.target[2]}학년`} &nbsp;
         {infos.classification} &nbsp;
-        {infos.class_time}
+        {getTimetableClassName(infos.class_time)}
       </OtherLabels>
       <Rating>{infos.rating.toFixed(1)}</Rating>
       {isHovered && (
@@ -62,6 +63,46 @@ const Lecture = ({ infos }) => {
       )}
     </Background>
   );
+};
+
+/**
+ * [101, 102, 103, 104] 처럼 넘어오는 classTime을
+ * 화 1B~3A 와 같은 이름으로 변경합니다.
+ */
+const getTimetableClassName = (classTime) => {
+  const distributedClassTime = distributeClassTime(JSON.parse(classTime));
+
+  let classNames = distributedClassTime.reduce((acc, time) => {
+    let startTime = time[0].toString().padStart(3, "0");
+    let endTime = time[time.length - 1].toString().padStart(3, "0");
+
+    const day = getDay(startTime[0]);
+    const startName = getTime(startTime.slice(1));
+    const endName = getTime(endTime.slice(1));
+
+    acc.push(`${day} ${startName}~${endName}`);
+    return acc;
+  }, []);
+
+  return classNames.join(", ");
+};
+
+const getDay = (dayStringData) => {
+  const dayList = {
+    0: "월",
+    1: "화",
+    2: "수",
+    3: "목",
+    4: "금",
+  };
+  return dayList[dayStringData];
+};
+
+const getTime = (timeStringData) => {
+  const convertedTime = parseInt(timeStringData);
+  const time = Math.floor(convertedTime / 2) + 1;
+  const alphabet = convertedTime % 2 === 0 ? "A" : "B";
+  return `${time}${alphabet}`;
 };
 
 const requestReflectLecture = async (lectureInfo, userTimetableId, dispatch) => {
