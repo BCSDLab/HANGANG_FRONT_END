868a8bf1bf9216ff6866e5e9e185257184445fad
jonghopark95 <jong951005@gmail.com>
2021-06-14 02:21:22 +0900

    refactor: Move timetable modal to timetablepage.js

--- src/containers/Shared/ModalProvider.js
+++ src/containers/Shared/ModalProvider.js
@@ -2,8 +2,6 @@ import React from "react";
 import AlertModalComponent from "components/ModalComponents/AlertModalComponent";
 import ConfirmModalComponent from "components/ModalComponents/ConfirmModalComponent";
 import ReportModalComponent from "components/ModalComponents/ReportModalComponent";
-import AddTimetableComponent from "components/ModalComponents/TimetablePage/AddTimetableComponent";
-import TimetableMoreComponent from "components/ModalComponents/TimetablePage/TimetableMoreComponent";
 
 const ModalProvider = ({ children }) => (
   <>
@@ -11,8 +9,6 @@ const ModalProvider = ({ children }) => (
     <AlertModalComponent />
     <ConfirmModalComponent />
     <ReportModalComponent />
-    <AddTimetableComponent />
-    <TimetableMoreComponent />
   </>
 );
 
--- src/pages/TimeTablePage.js
+++ src/pages/TimeTablePage.js
@@ -1,8 +1,18 @@
 import React from "react";
 import TimetablePageContainer from "containers/TimetableContainers";
+import AddTimetableComponent from "components/ModalComponents/TimetablePage/AddTimetableComponent";
+import TimetableMoreComponent from "components/ModalComponents/TimetablePage/TimetableMoreComponent";
+import LectureInfoModalComponent from "components/ModalComponents/TimetablePage/LectureInfoModalComponent";
 
 const TimetablePage = () => {
-  return <TimetablePageContainer />;
+  return (
+    <>
+      <TimetablePageContainer />
+      <AddTimetableComponent />
+      <TimetableMoreComponent />
+      <LectureInfoModalComponent />
+    </>
+  );
 };
 
 export default TimetablePage;
