de519a2bd5d7bae03bc9a88a946e69af844a60b3
jonghopark95 <jong951005@gmail.com>
2021-06-25 13:59:08 +0900

    style: Move out styling code on MajorSearchContainer

--- src/containers/IndexContainers/MajorSearchContainer.js
+++ src/containers/IndexContainers/MajorSearchContainer.js
@@ -1,48 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import styled from "styled-components";
 
-import { FontColor } from "static/Shared/commonStyles";
+import {
+  Label,
+  Content,
+  MajorCard,
+  MajorLabel,
+} from "containers/IndexContainers/styles/MajorSearchContainer.style";
 import MajorInfoArray from "static/IndexPage/majorInfoArray";
-
 import { setDepartmentOnLectures } from "store/modules/lecturesModule";
-
-const Label = styled.label`
-  color: ${FontColor};
-  font-size: 16px;
-  font-weight: 500;
-`;
-
-const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-top: 16px;
-`;
-
-const MajorCard = styled(Link)`
-  all: unset;
-  position: relative;
-  width: 100px;
-  height: 87px;
-  background-image: url(${({ imagesrc }) => imagesrc});
-  background-size: contain;
-  background-repeat: no-repeat;
-  cursor: pointer;
-`;
-
-const MajorLabel = styled.label`
-  position: absolute;
-  left: calc(50% - 34px);
-  bottom: 5px;
-  width: 68px;
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 1.4;
-  color: #fff;
-  text-align: center;
-`;
 
 /**
  * MajorSearchContainer
---
+++ src/containers/IndexContainers/styles/MajorSearchContainer.style.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import { FontColor } from "static/Shared/commonStyles";
+
+export const Label = styled.label`
+  color: ${FontColor};
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 16px;
+`;
+
+export const MajorCard = styled(Link)`
+  all: unset;
+  position: relative;
+  width: 100px;
+  height: 87px;
+  background-image: url(${({ imagesrc }) => imagesrc});
+  background-size: contain;
+  background-repeat: no-repeat;
+  cursor: pointer;
+`;
+
+export const MajorLabel = styled.label`
+  position: absolute;
+  left: calc(50% - 34px);
+  bottom: 5px;
+  width: 68px;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 1.4;
+  color: #fff;
+  text-align: center;
+`;
