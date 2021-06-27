5b24c0882d39d4fedfcacd44a27f5618bc24b352
jonghopark95 <jong951005@gmail.com>
2021-06-27 23:14:52 +0900

    build: Change build options for dev, production mode

---
+++ config/webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+const MODE = process.env.NODE_ENV;
+const ENTRY = path.join(__dirname, "..", "src", "index.js");
+const OUTPUT = path.join(__dirname, "..", "build");
+
+module.exports = {
+  entry: ENTRY,
+  output: {
+    path: OUTPUT,
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new Dotenv({
+      path: path.join(__dirname, "..", `.env.${MODE}`),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
