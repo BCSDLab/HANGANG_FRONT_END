3bc59233210e25e8028e238a3fdf668772c110c1
jonghopark95 <jong951005@gmail.com>
2021-06-27 10:27:25 +0900

    refactor: Refactoring on point section

--- src/components/MyPageComponents/PointSection.js
+++ src/components/MyPageComponents/PointSection.js
@@ -1,155 +1,35 @@
 import React from "react";
-import styled from "styled-components";
-import PropTypes from "prop-types";
-
-import {
-  BorderColor,
-  ConceptColor,
-  FontColor,
-  MyPageSectionHeight,
-  PlaceholderColor,
-} from "static/Shared/commonStyles";
-import { PointGuidanceArr } from "static/MyPage/PointGuidance";
 import { useSelector } from "react-redux";
 
-const Section = styled.section`
-  min-height: ${MyPageSectionHeight};
-  padding-top: 48px;
-`;
-
-const Label = styled.label`
-  color: ${ConceptColor};
-  font-size: 16px;
-  font-weight: 500;
-  margin-bottom: 16px;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 40px;
-`;
-
-const SystemGuidance = styled.span`
-  color: ${PlaceholderColor};
-  font-size: 15px;
-  font-weight: normal;
-  line-height: 1.5;
-`;
-
-const UsageGuidance = styled.div`
-  width: 270px;
-  height: 28px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Mark = styled.mark`
-  all: unset;
-  color: ${FontColor};
-  font-size: 15px;
-  line-height: 28px;
-`;
-
-const PointGuidance = styled.span`
-  color: ${PlaceholderColor};
-  font-size: 14px;
-  line-height: 28px;
-`;
-
-const TotalLabel = styled(Label)`
-  position: absolute;
-  right: 0;
-  font-size: 16px;
-  color: ${FontColor};
-`;
-
-const BreakdownWrapper = styled.div`
-  overflow-y: scroll;
-  height: 290px;
-  padding-top: 16px;
-  margin-top: 16px;
-  border-top: 1px solid ${BorderColor};
-
-  -ms-overflow-style: none; // IE and Edge
-  scrollbar-width: none; // Firefox
-  ::-webkit-scrollbar {
-    display: none; // Chrome
-  }
-`;
-
-const Breakdown = styled.div`
-  position: relative;
-  height: 40px;
-  margin-bottom: 24px;
-`;
-
-const Title = styled.span`
-  font-size: 15px;
-  color: ${FontColor};
-`;
-
-const CreatedAt = styled.span`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  font-size: 12px;
-  color: ${PlaceholderColor};
-`;
-
-const Variance = styled.span`
-  position: absolute;
-  top: calc(50% - 7.5px);
-  right: 0;
-  height: 15px;
-  font-size: 16px;
-  font-weight: 500;
-  color: ${PlaceholderColor};
-`;
+import {
+  Section,
+  Wrapper,
+  Label,
+  SystemGuidance,
+  UsageGuidance,
+  Mark,
+  PointGuidance,
+  PointBreakdownSection,
+  TotalLabel,
+  BreakdownWrapper,
+  Breakdown,
+  Title,
+  CreatedAt,
+  Variance,
+} from "components/MyPageComponents/styles/PointSection.style";
+import { PointGuidanceArr } from "static/MyPage/PointGuidance";
 
 const PointSection = () => {
   const { pointRecords, infos } = useSelector((state) => state.myPageReducer);
 
-  /**
-   * DB에 저장되있는 time을 기획에 맞게 보여줍니다.
-   * @param {string} createdAt
-   */
-  const createdAtConverter = (createdAt) => {
-    const splitedTime = createdAt.split(".")[0].split("T");
-    let date = splitedTime[0].split("-");
-    let time = splitedTime[1].split(":");
-    date = `${date[1]}/${date[2]} `;
-    time = `${time[0]}:${time[1]}`;
-    return date + time;
-  };
-
-  /**
-   * - 를 포함하지 않는다면 + 를 붙여주고,
-   * 아니라면 - 를 붙여줍니다.
-   * @param {number} variance
-   */
-  const varianceConverter = (variance) => {
-    if (!String(variance).includes("-")) {
-      return `+ ${variance}P`;
-    } else {
-      return `- ${String(variance).slice(1)}P`;
-    }
-  };
-
   return (
     <Section>
       <Wrapper>
-        <Label>포인트 시스템 안내</Label>
-        <SystemGuidance>
-          포인트 시스템은 강의평가 서비스와 강의자료 서비스에서 사용하는 가상 포인트를
-          적립하고, 사용하는 시스템입니다.
-          <br /> 포인트 획득을 위해 허위 / 중복 / 성의없는 정보를 작성할 경우, 허위
-          신고하는 경우, 그 외 부적절한 방법으로 포인트 시스템을 남용하는 경우에 서비스
-          이용이 영구 제한될 수 있습니다.
-        </SystemGuidance>
+        <Label>{POINT_SYSTEM_LABEL}</Label>
+        <SystemGuidance>{POINT_SYSTEM_GUIDE}</SystemGuidance>
       </Wrapper>
       <Wrapper>
-        <Label>{"포인트 적립 & 사용"}</Label>
+        <Label>{POINT_USAGE_LABEL}</Label>
         {PointGuidanceArr.map(({ guidance, point }) => (
           <UsageGuidance key={guidance}>
             <Mark>{guidance}</Mark>
@@ -157,21 +37,57 @@ const PointSection = () => {
           </UsageGuidance>
         ))}
       </Wrapper>
-      <div style={{ position: "relative", marginBottom: "24px" }}>
-        <Label>내 포인트 내역</Label>
-        <TotalLabel>{`합계 : ${infos.point}P`}</TotalLabel>
+      <PointBreakdownSection>
+        <Label>{POINT_BREAKDOWN_LABEL}</Label>
+        <TotalLabel>{getTotalPointLabel(infos.point)}</TotalLabel>
         <BreakdownWrapper>
-          {pointRecords.map(({ id, variance, title, created_at }) => (
-            <Breakdown key={id}>
-              <Title>{title}</Title>
-              <CreatedAt>{createdAtConverter(created_at)}</CreatedAt>
-              <Variance>{varianceConverter(variance)}</Variance>
-            </Breakdown>
-          ))}
+          {pointRecords
+            .sort(({ id }) => -id)
+            .map(({ id, variance, title, created_at }) => (
+              <Breakdown key={id}>
+                <Title>{title}</Title>
+                <CreatedAt>{createdAtConverter(created_at)}</CreatedAt>
+                <Variance>{varianceConverter(variance)}</Variance>
+              </Breakdown>
+            ))}
         </BreakdownWrapper>
-      </div>
+      </PointBreakdownSection>
     </Section>
   );
 };
+
+/**
+ * DB에 저장되있는 time을 기획에 맞게 보여줍니다.
+ * @param {string} createdAt
+ */
+const createdAtConverter = (createdAt) => {
+  const splitedTime = createdAt.split(".")[0].split("T");
+  let date = splitedTime[0].split("-");
+  let time = splitedTime[1].split(":");
+  date = `${date[1]}/${date[2]} `;
+  time = `${time[0]}:${time[1]}`;
+  return date + time;
+};
+
+/**
+ * - 를 포함하지 않는다면 + 를 붙여주고,
+ * 아니라면 - 를 붙여줍니다.
+ * @param {number} variance
+ */
+const varianceConverter = (variance) => {
+  if (!String(variance).includes("-")) {
+    return `+ ${variance}P`;
+  } else {
+    return `- ${String(variance).slice(1)}P`;
+  }
+};
+
+const getTotalPointLabel = (point) => `합계 : ${point}P`;
+
+const POINT_SYSTEM_LABEL = "포인트 시스템 안내";
+const POINT_SYSTEM_GUIDE =
+  "포인트 시스템은 강의평가 서비스와 강의자료 서비스에서 사용하는 가상 포인트를 적립하고, 사용하는 시스템입니다.\n포인트 획득을 위해 허위 / 중복 / 성의없는 정보를 작성할 경우, 허위 신고하는 경우, 그 외 부적절한 방법으로 포인트 시스템을 남용하는 경우에 서비스 이용이 영구 제한될 수 있습니다.";
+const POINT_USAGE_LABEL = "포인트 적립 & 사용";
+const POINT_BREAKDOWN_LABEL = "내 포인트 내역";
 
 export default PointSection;
