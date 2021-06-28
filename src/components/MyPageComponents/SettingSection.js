4cbef778a0ca697d214dda42ac293965a916ae6e
jonghopark95 <jong951005@gmail.com>
2021-06-29 00:50:03 +0900

    style: Move out style codes on setting section

--- src/components/MyPageComponents/SettingSection.js
+++ src/components/MyPageComponents/SettingSection.js
@@ -1,196 +1,31 @@
 import React, { useState } from "react";
 import { majorsFullName } from "static/MyPage/majors";
 import {
-  AlertColor,
-  BorderColor,
-  ButtonColor,
-  ConceptColor,
-  CopyRightColor,
-  FontColor,
-  MyPageSectionHeight,
-  PlaceholderColor,
-} from "static/Shared/commonStyles";
-import styled from "styled-components";
+  SettingSectionWrapper,
+  Profile,
+  Label,
+  ProfileLeftSection,
+  SubLabel,
+  ProfileInput,
+  AlertLabel,
+  NicknameInputWrapper,
+  NicknameModifySection,
+  ModifyButton,
+  AbleButton,
+  ProfileRightSection,
+  MajorSubLabel,
+  MajorChoiceSection,
+  Setting,
+  Row,
+  NotifyLabel,
+  ToggleButton,
+  Circle,
+  VersionNotify,
+  Etc,
+  RightButton,
+  WithdrawalButton,
+} from "components/MyPageComponents/styles/SettingSection.style";
 import { getValueOnLocalStorage, setValueOnLocalStorage } from "utils/localStorageUtils";
-
-const SettingSectionWrapper = styled.div`
-  min-height: ${MyPageSectionHeight};
-  padding-top: 48px;
-`;
-
-const Profile = styled.div`
-  width: 100%;
-`;
-
-const Setting = styled.div`
-  width: 100%;
-  margin-top: 40px;
-`;
-
-const Etc = styled(Setting)``;
-
-const ProfileLeftSection = styled.div`
-  width: 547px;
-  margin-top: 16px;
-  margin-right: 112px;
-`;
-
-const ProfileInput = styled.input`
-  all: unset;
-  height: 22px;
-  width: 547px;
-  margin: 16px 0px 32px 0px;
-  border-bottom: 1px solid ${BorderColor};
-  font-size: 15px;
-`;
-
-const NicknameInputWrapper = styled.div`
-  position: relative;
-`;
-
-const AlertImg = styled.img.attrs({
-  src: "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/alert.png",
-  alt: "경고 이미지",
-})`
-  position: absolute;
-  right: 33px;
-  bottom: 4px;
-  width: 20px;
-  height: 20px;
-`;
-
-const NicknameModifySection = styled.div`
-  position: absolute;
-  right: 0;
-  bottom: 6px;
-
-  cursor: pointer;
-`;
-
-const ModifyButton = styled.button`
-  all: unset;
-  font-size: 14px;
-  color: ${PlaceholderColor};
-`;
-
-const AbleButton = styled(ModifyButton)`
-  color: ${ConceptColor};
-`;
-
-const MajorChoiceSection = styled.div`
-  display: grid;
-  grid-template-columns: 176px 176px;
-  grid-template-rows: 29px 29px 29px 29px;
-  gap: 24px 16px;
-  margin-top: 16px;
-`;
-
-const Major = styled.input.attrs(() => ({
-  type: "button",
-}))`
-  width: 176px;
-  height: 29px;
-  line-height: 0px;
-  outline: none;
-  border: none;
-  border-radius: 24px;
-  color: ${({ choiced }) => (choiced ? "#fff" : `${FontColor}`)};
-  background-color: ${({ choiced }) => (choiced ? `${ButtonColor}` : `${BorderColor}`)};
-  font-size: 14px;
-  font-weight: normal;
-  cursor: pointer;
-`;
-
-const ProfileRightSection = styled.div`
-  width: calc(100% - 547px);
-  margin-top: 16px;
-`;
-
-const Label = styled.label`
-  color: ${ConceptColor};
-  font-size: 16px;
-  font-weight: 500;
-`;
-
-const SubLabel = styled(Label)`
-  color: ${FontColor};
-`;
-
-const NotifyLabel = styled(Label)`
-  color: ${FontColor};
-  line-height: 25px;
-  font-weight: normal;
-`;
-
-const VersionNotify = styled.span`
-  position: absolute;
-  right: 0;
-  color: ${PlaceholderColor};
-  font-size: 14px;
-  line-height: 24px;
-`;
-
-const MajorSubLabel = styled.span`
-  margin-left: 12px;
-  color: ${PlaceholderColor};
-  font-size: 12px;
-  font-weight: normal;
-`;
-
-const AlertLabel = styled.span`
-  margin-left: 8px;
-  color: ${AlertColor};
-  font-size: 11px;
-  font-weight: normal;
-`;
-
-const Row = styled.div`
-  height: 24px;
-  position: relative;
-`;
-
-const ToggleButton = styled.button`
-  all: unset;
-  position: absolute;
-  top: 0;
-  right: 0;
-  display: flex;
-  padding: 0px 2px;
-  align-items: center;
-  width: 48px;
-  height: 24px;
-  border-radius: 12px;
-  background-color: ${({ status }) => (status ? `${ConceptColor}` : `${CopyRightColor}`)};
-  cursor: pointer;
-`;
-
-const Circle = styled.div`
-  position: absolute;
-  right: 2px;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: #fff;
-  transition: all 0.2s ease;
-  transform: translateX(${({ status }) => (status ? 0 : -28)}px);
-  /* transform: translateX(-28px); */
-`;
-
-const RightButton = styled.img.attrs({
-  src:
-    "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/mypage/right.png",
-  alt: "오른쪽 화살표",
-})`
-  position: absolute;
-  top: 2px;
-  right: 0;
-  width: 20px;
-`;
-
-const WithdrawalButton = styled.button`
-  all: unset;
-  cursor: pointer;
-`;
 
 const SettingSection = ({
   nicknameTest,
