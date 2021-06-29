e260cfd2cb87fa2999a9500e2f56d3b33e32b38a
jonghopark95 <jong951005@gmail.com>
2021-06-29 12:27:35 +0900

    style: Move out styling codes on lecture card

--- src/components/Shared/LectureCard.js
+++ src/components/Shared/LectureCard.js
@@ -1,97 +1,19 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-
 import {
-  BorderColor,
-  ConceptColor,
-  FontColor,
-  PlaceholderColor,
-} from "static/Shared/commonStyles";
-
-const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  width: 557px;
-  height: 133px;
-  padding: 24px 0px 16px 28px;
-  border-radius: 8px;
-  border: solid 1px ${BorderColor};
-  background-color: ${({ isChosen }) => (isChosen ? `${BorderColor}` : "#fff")};
-  cursor: pointer;
-`;
-
-const LeftSide = styled.div`
-  position: relative;
-  display: flex;
-  height: 100%;
-  width: 447px;
-  flex-direction: column;
-`;
-
-const RightSide = styled.div`
-  position: relative;
-  width: calc(555px - 447px);
-  right: 28px;
-`;
-
-const Title = styled.span`
-  font-size: 16px;
-  font-weight: 500;
-  color: ${FontColor};
-`;
-
-const Amount = styled.span`
-  font-size: 13px;
-  color: ${PlaceholderColor};
-  margin-left: 4px;
-`;
-
-const Professor = styled(Title)`
-  font-weight: normal;
-  margin-top: 16px;
-`;
-
-const HashTagWrapper = styled.div`
-  position: absolute;
-  bottom: 0;
-  display: flex;
-
-  margin-top: 16px;
-`;
-
-const HashTag = styled.span`
-  font-size: 14px;
-  color: ${PlaceholderColor};
-  margin-right: 7px;
-`;
-
-const Classification = styled.span`
-  position: absolute;
-  right: 0;
-  font-size: 14px;
-  color: ${ConceptColor};
-`;
-
-const Rating = styled.span`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  font-size: 22px;
-  color: ${FontColor};
-`;
-
-const Bookmark = styled.img.attrs({
-  src:
-    "https://hangang-storage.s3.ap-northeast-2.amazonaws.com/assets/img/mypage/bookmark.png",
-  alt: "북마크",
-})`
-  position: absolute;
-  top: -8px;
-  left: 0;
-  width: 32px;
-`;
+  Wrapper,
+  LeftSide,
+  RightSide,
+  Title,
+  Amount,
+  Professor,
+  HashTagWrapper,
+  HashTag,
+  Classification,
+  Rating,
+  Bookmark,
+} from "components/Shared/styles/LectureCard.style";
 
 /**
  * LectureCard
@@ -106,12 +28,6 @@ const LectureCard = ({
   ...rest
 }) => {
   const history = useHistory();
-  const titleSlicer = (title) => {
-    if (title.length > 26) {
-      title = title.slice(0, 26) + "...";
-    }
-    return title;
-  };
 
   return (
     <Wrapper
@@ -146,12 +62,11 @@ const LectureCard = ({
   );
 };
 
-LectureCard.defaultProps = {
-  isScrapped: false,
-  isChosen: false,
-  isEditMode: false,
-  chooseScrap: () => {},
-  rest: {},
+const titleSlicer = (title) => {
+  if (title.length > 26) {
+    title = title.slice(0, 26) + "...";
+  }
+  return title;
 };
 
 LectureCard.propTypes = {
