f0a3983d96e27ffa804a24ec64a36ba0fb3f8a69
jonghopark95 <jong951005@gmail.com>
2021-07-01 01:28:30 +0900

    style: Move out styling codes on attachment

--- src/components/ResourceDetailComponents/Attachment.js
+++ src/components/ResourceDetailComponents/Attachment.js
@@ -1,45 +1,14 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
+
 import PropTypes from "prop-types";
 
 import ResourceDetailAPI from "api/resourceDetail";
-import {
-  notPurchasedIconPath,
-  purchasedIconPath,
-} from "static/ResourceDetailPage/imgPath";
-import { BorderColor, FontColor, PlaceholderColor } from "static/Shared/commonStyles";
 import { getValueOnLocalStorage } from "utils/localStorageUtils";
 import { showAlertModal } from "store/modules/modalModule";
 import ALERT_MESSAGE_ON_ERROR_TYPE from "static/Shared/ALERT_MESSAGE_ON_ERROR_TYPE";
 
-const Wrapper = styled.div`
-  position: relative;
-  min-width: 90px;
-  height: 90px;
-  border-radius: 8px;
-  border: 1px solid ${BorderColor};
-
-  cursor: ${({ isPurchased }) => (isPurchased ? "pointer" : "default")};
-`;
-
-const File = styled.img.attrs(({ ext, isPurchased }) => ({
-  src: isPurchased ? purchasedIconPath(ext) : notPurchasedIconPath(ext),
-  alt: "file",
-}))`
-  position: absolute;
-  top: 7px;
-  left: 7px;
-  width: 24px;
-`;
-
-const Name = styled.span`
-  position: absolute;
-  right: 7px;
-  bottom: 7px;
-  font-size: 12px;
-  color: ${({ isPurchased }) => (isPurchased ? `${FontColor}` : `${PlaceholderColor}`)};
-`;
+import { Wrapper, File, Name } from "./styles/Attachment.style";
 
 const convertName = (name, ext) => {
   if (name.length <= 7) return name.slice(0, 3) + "." + ext;
