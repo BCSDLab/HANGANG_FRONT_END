e7da885af361d19a8103a29e1d5bc18b17b00ac0
jonghopark95 <jong951005@gmail.com>
2021-06-30 22:52:35 +0900

    style: Move out styling codes on ResourceCard

--- src/components/ResourceComponents/ResourceCard.js
+++ src/components/ResourceComponents/ResourceCard.js
@@ -1,126 +1,28 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import ResourceAPI from "api/resources";
 import { SERVICE_NEEDED_LOGIN } from "static/ErrorComments";
-import {
-  BorderColor,
-  ConceptColor,
-  FontColor,
-  PlaceholderColor,
-} from "static/Shared/commonStyles";
 import { getValueOnLocalStorage } from "utils/localStorageUtils";
-import { NOT_PUSHED_THUMB_URL, PUSHED_THUMB_URL } from "static/Shared/imageUrls";
 
-const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  width: 557px;
-  height: 133px;
-  border-radius: 8px;
-  border: solid 1px ${BorderColor};
-  background-color: "#fff";
-  cursor: pointer;
-`;
-const Thumbnail = styled.div`
-  width: 131px;
-  border-top-left-radius: 6px;
-  border-bottom-left-radius: 6px;
-
-  /* background */
-  background-image: url(${({ uri }) => uri});
-  background-repeat: no-repeat;
-  background-size: 65px;
-  background-position: center;
-`;
-
-const Content = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  width: calc(100% - 131px);
-  padding: 20px 17px;
-`;
-
-const Title = styled.span`
-  font-size: 16px;
-  font-weight: 500;
-  color: ${FontColor};
-`;
-
-const Nickname = styled(Title)`
-  font-weight: normal;
-  margin-top: 13px;
-`;
-
-const LectureInfos = styled.div`
-  margin-top: 32px;
-  display: flex;
-  align-items: center;
-`;
-
-const LectureName = styled(Link)`
-  all: unset;
-  font-size: 14px;
-  color: ${PlaceholderColor};
-  cursor: pointer;
-`;
-
-const Delimiter = styled.div`
-  width: 1px;
-  height: 8px;
-  margin: 0px 8px;
-  background-color: ${PlaceholderColor};
-`;
-
-const LectureProfessor = styled(LectureName)`
-  cursor: default;
-`;
-
-const Category = styled.span`
-  position: absolute;
-  top: 21px;
-  right: 15px;
-  font-size: 14px;
-  color: ${ConceptColor};
-`;
-
-const HitWrapper = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  bottom: 20px;
-  right: 15px;
-`;
-
-const HitAmount = styled.div`
-  margin-left: 4px;
-  font-size: 16px;
-  color: ${({ isHit }) => (isHit ? `${ConceptColor}` : `${PlaceholderColor}`)};
-`;
-
-const HitIcon = styled.img.attrs(({ isHit }) => ({
-  src: isHit ? PUSHED_THUMB_URL : NOT_PUSHED_THUMB_URL,
-  alt: "hit_icon",
-}))`
-  width: 19px;
-  cursor: pointer;
-`;
-
-/**
- * Return the sliced string that maximum length is 'max'.
- * @param string A string needed to slice.
- * @param max A number that set maximum value to slice string.
- */
-const sliceString = (string, max) => {
-  if (string.length > max) {
-    string = string.slice(0, max) + "...";
-  }
-  return string;
-};
+import {
+  Wrapper,
+  Thumbnail,
+  Content,
+  Title,
+  Nickname,
+  LectureInfos,
+  LectureName,
+  Delimiter,
+  LectureProfessor,
+  Category,
+  HitWrapper,
+  HitAmount,
+  HitIcon,
+} from "./styles/ResourceCard.style";
+import { convertHTMLEntities } from "utils/convertHTMLEntities";
 
 /**
  * Request "Hit API" when user pushed hit icon.
@@ -166,7 +68,7 @@ const ResourceCard = ({ data: { is_hit, hits, ...rest } }) => {
     <Wrapper onClick={() => history.push(`/resource/${rest.id}`)}>
       <Thumbnail uri={rest.thumbnail} />
       <Content>
-        <Title>{sliceString(rest.title, 26)}</Title>
+        <Title>{convertHTMLEntities(sliceString(rest.title, 26))}</Title>
         <Nickname>{rest.user.nickname}</Nickname>
         <LectureInfos>
           <LectureName to={`lectures/${rest.lecture_id}`}>
@@ -192,6 +94,18 @@ const ResourceCard = ({ data: { is_hit, hits, ...rest } }) => {
       </Content>
     </Wrapper>
   );
+};
+
+/**
+ * Return the sliced string that maximum length is 'max'.
+ * @param string A string needed to slice.
+ * @param max A number that set maximum value to slice string.
+ */
+const sliceString = (string, max) => {
+  if (string.length > max) {
+    string = string.slice(0, max) + "...";
+  }
+  return string;
 };
 
 ResourceCard.defaultProps = {
