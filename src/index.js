185516052a1c5415b462ff94795d354c291c921a
Yunsup Sim <pedogunu@gmail.com>
2021-07-19 17:14:45 +0900

    feat: Add firebase setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+import App from "App";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "GlobalStyle";
+import ModalProvider from "containers/Shared/ModalProvider";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDom from "react-dom";
-import { Provider } from "react-redux";
 import { ToastProvider } from "react-toast-notifications";
+import { initializeAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import store from "store";
-import App from "App";
-import GlobalStyle from "GlobalStyle";
-import { BrowserRouter } from "react-router-dom";
-import ModalProvider from "containers/Shared/ModalProvider";
+
+if (process.env.NODE_ENV === "production") {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDZSkOmK1P8UCIVQXsAKp6geMOeR2RtUas",
+    authDomain: "hangang-e3330.firebaseapp.com",
+    projectId: "hangang-e3330",
+    storageBucket: "hangang-e3330.appspot.com",
+    messagingSenderId: "642816113964",
+    appId: "1:642816113964:web:a924bbf0ff0432f5e94089",
+    measurementId: "G-6557HN7M06"
+  }
+  const firebaseApp = initializeApp(firebaseConfig);
+  initializeAnalytics(firebaseApp);
+}
 
 ReactDom.render(
   <Provider store={store}>
