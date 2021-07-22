89987f7f58ee7091381ae978d4bd9a08d524eb01
Yunsup Sim <pedogunu@gmail.com>
2021-07-22 21:43:56 +0900

    feat: Add rating radio

---
+++ src/components/ModalComponents/LectureDetailPage/LectureRating/LectureRating.js
@@ -0,0 +1,32 @@
+import LectureRatingStar from '../LectureRatingStar/LectureRatingStar';
+import React from 'react';
+import styled from 'styled-components';
+
+export const LectureRatingContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 200px;
+`;
+
+const LectureRating = (_, ref) => {
+    const [starReview, setStarReview] = React.useState(6);
+    React.useImperativeHandle(ref, () => ({
+        value: starReview,
+    }), [starReview]);
+
+    return (
+        <LectureRatingContainer>
+            {Array.from({length: 5}, (_, index) => (index))
+                .map((value) => (
+                    <LectureRatingStar
+                        key={value}
+                        starReview={starReview}
+                        setStarReview={setStarReview}
+                        index={value} />
+                )
+            )}
+        </LectureRatingContainer>
+    );
+};
+
+export default React.forwardRef(LectureRating);
